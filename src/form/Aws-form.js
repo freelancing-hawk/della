94e6ea7dac6b7a1aef0cf5e362bdc182a158a5c6
Rokkam Padma Shyam Sundar <rokkampadmashyamsundar.a@infoview.co.jp>
2020-05-30 10:09:54 +0530

    page design commit

--- src/form/Aws-form.js
+++ src/form/Aws-form.js
@@ -4,15 +4,24 @@ import ReactDOM from 'react-dom';
 
 const AwsForm = () => {
     return (
-        <div className = "google-form" >
-            <Input label="AWS Access Key" id="aws-access-key" type="text"/>
-            <Input label="AWS Secret Key" id="aws-secret-key" type="text"/>
-            <button onClick={changeDom}>Sign In</button>
+        <div>
+            <div className="Aws-form row px-5 pt-5 text-secondary">
+                <h3>AWS</h3>
+            </div>
+            <div class="row px-3 bg-white border border-secondry">
+                <Input label="AWS Access Key" id="aws-access-key" type="text" />
+                <Input label="AWS Secret Key" id="aws-secret-key" type="text" />
+                <div className="row mx-3 w-100 d-flex jusitfy-content-center ">
+                    <div className="col  p-3">
+                        <button className="btn btn-primary" onClick={changeDom}>Sign In</button>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 }
 
-const changeDom = () =>{
+const changeDom = () => {
     //ReactDOM.render(<App />, document.getElementById('root'));
 }
 
--- src/form/Input.js
+++ src/form/Input.js
@@ -2,10 +2,14 @@ import React from 'react';
 
 const Input = (props) => {
     return (
-    <div className = "input-type">
-        <label>{props.label}</label>
-        <input type={props.type} id={props.id} />
-    </div>
+        <div className="row mx-3 w-100">
+            <div className="col-2 mx-3 p-3 d-flex align-items-center">
+                <label>{props.label}</label>
+            </div>
+            <div className="col input-group p-3">
+                <input type={props.type} id={props.id} className="form-control" aria-label="Default" aria-describedby="inputGroup-sizing-default" />
+            </div>
+        </div>
     );
 };
 
